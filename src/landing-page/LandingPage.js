5844002f7e057a96e952b20669286db16184b56c
Adrian T (Big Coder Man) <adriantarantino2006@gmail.com>
2023-06-30 14:59:45 -0400

    Adding Icons and Fixing bug with background image

--- src/landing-page/LandingPage.js
+++ src/landing-page/LandingPage.js
@@ -1,23 +1,57 @@
-import LandingPageBackground from './images/LandingPageBackground.jpg'
-import './LandingPage.css'
+import LandingPageBackground from './images/background/LandingPageBackground.jpg';
+import ArduinoImageTB from './images/icons/transparent-background/ArduinoImageTB.png';
+import JavaImageTB from './images/icons/transparent-background/JavaImageTB.png';
+import PythonImageTB from './images/icons/transparent-background/PythonImageTB.png';
+import ReactImageTB from './images/icons/transparent-background/ReactImageTB.png';
+import RustImageTB from './images/icons/transparent-background/RustImageTB.png';
+import './LandingPage.css';
 
 function Title() {
     return (
         <h1 className='title'>Adrian Tarantino</h1>
-    )
+    );
 }
 
 function Description() {
-    const descriptionText = "Hello, I'm Adrian. I've been coding for roughly six years in a variety of different languages and frameworks. Below are some of the technologies I am most familiar with:";
+    const descriptionText = "Hello, I'm Adrian. I've been coding since since roughly 2017. Over the years, I've coded in a variety of different languages and frameworks. Below are some of the technologies I am most familiar with:";
     return (
         <p className='description'>{descriptionText}</p>
-    )
+    );
 }
 
-function Icon({ size }) {}
-function IconRow() {}
+function Icon({ imagePath, iconName }) {
+    return (
+        <td>
+            <div className={'icon'}>
+                <img className='icon-image' src={imagePath} alt={iconName} width='100'/>
+            </div>
+        </td>
+    );
+}
+
+function IconRow() {
+    let icons = [<Icon imagePath={ArduinoImageTB} iconName='Arduino'/>,
+                 <Icon imagePath={JavaImageTB} iconName='Java'/>,
+                 <Icon imagePath={PythonImageTB} iconName='Python'/>,
+                 <Icon imagePath={ReactImageTB} iconName='React'/>,
+                 <Icon imagePath={RustImageTB} iconName='Rust'/>];
+
+    return (
+        <table className='icon-row'>
+            <tbody>
+                {icons}
+            </tbody>
+        </table>
+    );
+}
 
 function LandingPageBase() {
+    const iconImagePaths = [ArduinoImageTB,
+                            JavaImageTB,
+                            PythonImageTB,
+                            ReactImageTB,
+                            RustImageTB];
+
     return (
         <div className='landing-page-base'>
             <img className='background-image' src={LandingPageBackground}/>
@@ -27,7 +61,7 @@ function LandingPageBase() {
                 <IconRow/>
             </div>
         </div>
-    )
+    );
 }
 
 export default LandingPageBase;
