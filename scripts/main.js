7fbf6f0fb021514f37bc4b918fce100fe1672f34
AdrianTheHacker <57376726+AdrianTheHacker@users.noreply.github.com>
2024-05-18 21:14:50 -0400

    Adding typing animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,45 @@
-import { createRoot } from 'react-dom/client';
+var app = document.getElementById('animation-text');
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
 
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+const frcString = 'FIRST robotics alumni';
+const educationString = 'grade 12 @ Garth Webb SS';
+const jobString = 'lifeguard and swim instructor';
+const electronicsString = 'electronics enthusiast';
+const programmingString = 'hobby programmer';
+
+const startTime = 500;
+const fullTime = 300;
+const emptyTime = 500;
+
+typewriter
+  .pauseFor(startTime)
+  .typeString(educationString)
+  .pauseFor(fullTime)
+  .deleteChars(educationString.length)
+  .pauseFor(emptyTime)
+
+  .typeString(frcString)
+  .pauseFor(fullTime)
+  .deleteChars(frcString.length)
+  .pauseFor(emptyTime)
+  
+  .typeString(jobString)
+  .pauseFor(fullTime)
+  .deleteChars(jobString.length)
+  .pauseFor(emptyTime)
+
+  .typeString(electronicsString)
+  .pauseFor(fullTime)
+  .deleteChars(electronicsString.length)
+  .pauseFor(emptyTime)
+
+  .typeString(programmingString)
+  .pauseFor(fullTime)
+  .deleteChars(programmingString.length)
+  .pauseFor(emptyTime)
+
+  .start();
