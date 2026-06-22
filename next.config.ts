f9b6a269b02e8b70b9c03d26119915e0648bf2d3
AdrianTarantino1 <adrian.tarantino@qidni.com>
2026-06-22 09:41:07 -0400

    Making production branch

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const repoName = isProd ? '/AdrianTarantino.github.io' : '';
+
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
+  output: "export",
   reactStrictMode: true,
+
+  basePath: repoName,
+  assetPrefix: repoName,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
